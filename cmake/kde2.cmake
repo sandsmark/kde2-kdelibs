822bacd00ded13016cc6d64399bb182006cbbd67
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-10-26 14:53:28 +0100

    default to c++98

--- cmake/kde2.cmake
+++ cmake/kde2.cmake
@@ -23,4 +23,12 @@ include("${CMAKE_CURRENT_LIST_DIR}/kde2_utils.cmake")
 # The unfortunate global definitions
 add_definitions(-DQT_NO_TRANSLATION -DQT_CLEAN_NAMESPACE -DQT_NO_COMPAT -DQT_NO_ASCII_CAST)
 
+# Default to the standard closest to what was originally used
+set(CMAKE_CXX_STANDARD 98)
+set(CMAKE_CXX_EXTENSIONS ON)
 
+if(CMAKE_COMPILER_IS_GNUCXX)
+    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -Wno-write-strings")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_C_FLAGS} -Wno-write-strings")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -fpermissive")
+endif()
